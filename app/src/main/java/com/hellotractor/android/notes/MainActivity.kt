7d8b148bc65483347100941a7823e39b0f6fce2b
Eyosyas Fetene <eyosifetene98@gmail.com>
2026-03-12 10:53:49 +0300

    - Button to navigate to farm list

--- app/src/main/java/com/hellotractor/android/notes/MainActivity.kt
+++ app/src/main/java/com/hellotractor/android/notes/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.hellotractor.android.notes
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.hellotractor.android.notes.R
+import com.hellotractor.app.features.farms.FarmsFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Check if this is the first time the activity is created
+        if (savedInstanceState == null) {
+            // Optionally load a default fragment
+            // loadFragment(SomeOtherFragment())
         }
+
+        val btnViewFarms = findViewById<Button>(R.id.btn_view_farms)
+        btnViewFarms.setOnClickListener {
+            loadFragment(FarmsFragment())
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null) // This allows user to press back to return
+            .commit()
     }
 }
